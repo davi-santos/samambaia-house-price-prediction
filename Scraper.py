c70d1e4936fce732ace3270eac44fef430ae1594
Davi Santos <22971355+davi-santos@users.noreply.github.com>
2022-11-22 17:36:54 -0300

    go

--- Scraper.py
+++ Scraper.py
@@ -4,4 +4,27 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+PARAMS_REQUEST_HEADER =  {
+        'authority': 'df.olx.com.br',
+        'method': 'GET',
+        'path': '/imoveis/aluguel',
+        'scheme': 'https',
+        'referer': 'https://df.olx.com.br/imoveis/aluguel',
+        'sec-fetch-mode': 'navigate',
+}
 
+
+def searchData(neighbor = 'samambaia'):
+    url_base = 'https://df.olx.com.br/imoveis/venda?'
+    url_query_neighbor = 'q='+neighbor
+
+    url_=url_base + url_query_neighbor; x=0
+
+    while(x < 2):
+
+        x += 1
+
+
+
+    url_base = 'https://df.olx.com.br/imoveis/venda?q=samambaia'
+    url_2 = 'https://df.olx.com.br/imoveis/venda?o=2&q=samambaia'
